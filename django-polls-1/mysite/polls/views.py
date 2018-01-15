be9d7d7bd7e184b0b6f5daaff69032259553fc22
rodcoelho <rodcpython@gmail.com>
2018-01-15 10:09:01 -0500

    added new views + corresponding urls

--- django-polls-1/mysite/polls/views.py
+++ django-polls-1/mysite/polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World. You are at the polls page")
 
+def detail(request,question_id):
+    return HttpResponse("You're looking at question {}".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at results of question {}"
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}".format(question_id))
+
